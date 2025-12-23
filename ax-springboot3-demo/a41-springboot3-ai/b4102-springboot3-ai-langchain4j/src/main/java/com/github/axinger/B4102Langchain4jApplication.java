2f8596b9753958868dabd39980efe6925a959682
axinger <liu_weixing@qq.com>
2025-12-23 11:47:42 +0800

    update

---
+++ ax-springboot3-demo/a41-springboot3-ai/b4102-springboot3-ai-langchain4j/src/main/java/com/github/axinger/B4102Langchain4jApplication.java
@@ -0,0 +1,12 @@
+package com.github.axinger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B4102Langchain4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B4102Langchain4jApplication.class, args);
+    }
+}
