3423ddc2b1cd3173c7887920f7e07ace3aebfbe2
axinger <liu_weixing@qq.com>
2025-12-28 12:46:19 +0800

    update

--- ax-springboot2-demo/a01-spring-boot-learn/a01-02-java8/src/test/java/com/github/axinger/TreeSetTests.java
+++ ax-springboot2-demo/a01-spring-boot-learn/a01-02-java8/src/test/java/com/github/axinger/TreeSetTests.java
@@ -2,6 +2,7 @@ package com.github.axinger;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
@@ -34,5 +35,40 @@ public class TreeSetTests {
 
         Set<Object> set1 = set.stream().sorted().collect(Collectors.toSet());
         System.out.println("set1 = " + set1);
+    }
+
+    @Test
+    void test3() {
+        /// 向下取整, [a, b)
+        TreeSet<Integer> set = new TreeSet<>(Arrays.asList(10, 15, 20));
+
+        /// 返回 小于或等于 e 的最大元素, 向下对齐,包含
+        System.out.println(" set.floor(10) = " + set.floor(10));
+        System.out.println(" set.floor(14) = " + set.floor(14));
+        System.out.println(" set.floor(15) = " + set.floor(15));
+        System.out.println(" set.floor(16) = " + set.floor(16));
+        System.out.println(" set.floor(20) = " + set.floor(20));
+        System.out.println("===================================================");
+        /// 返回 大于或等于 e 的最小元素, 向上对齐,包含
+        System.out.println(" set.ceiling(10) = " + set.ceiling(10));
+        System.out.println(" set.ceiling(14) = " + set.ceiling(14));
+        System.out.println(" set.ceiling(15) = " + set.ceiling(15));
+        System.out.println(" set.ceiling(16) = " + set.ceiling(16));
+        System.out.println(" set.ceiling(20) = " + set.ceiling(20));
+        System.out.println("===================================================");
+        /// 返回 小于 e 的最大元素	❌ 不包含,向下对齐,不包含
+        System.out.println(" set.lower(10) = " + set.lower(10));
+        System.out.println(" set.lower(14) = " + set.lower(14));
+        System.out.println(" set.lower(15) = " + set.lower(15));
+        System.out.println(" set.lower(16) = " + set.lower(16));
+        System.out.println(" set.lower(20) = " + set.lower(20));
+
+        System.out.println("===================================================");
+        ///返回 大于 e 的最小元素	❌ 不包含,向上对齐,不包含
+        System.out.println(" set.higher(10) = " + set.higher(10));
+        System.out.println(" set.higher(14) = " + set.higher(14));
+        System.out.println(" set.higher(15) = " + set.higher(15));
+        System.out.println(" set.higher(16) = " + set.higher(16));
+        System.out.println(" set.higher(20) = " + set.higher(20));
     }
 }
