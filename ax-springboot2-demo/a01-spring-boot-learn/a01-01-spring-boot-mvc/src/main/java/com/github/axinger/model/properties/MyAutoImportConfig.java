b90b6f365cd71689934c4b0b0442ef7625631be5
axinger <liu_weixing@qq.com>
2026-06-05 10:44:58 +0800

    update

---
+++ ax-springboot2-demo/a01-spring-boot-learn/a01-01-spring-boot-mvc/src/main/java/com/github/axinger/model/properties/MyAutoImportConfig.java
@@ -0,0 +1,14 @@
+package com.github.axinger.model.properties;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(value = {
+        DemoUserProperties.class,
+        DemoBlockScalarProperties.class,
+        DemoPropertySourceProperties.class,
+        DemoConfigImportProperties.class
+})
+public class MyAutoImportConfig {
+}
