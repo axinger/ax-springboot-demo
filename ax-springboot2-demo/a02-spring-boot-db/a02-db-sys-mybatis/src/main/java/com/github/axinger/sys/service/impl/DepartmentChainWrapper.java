0902d37999ea46336288ef27334dcc4beddfc8f2
axinger <liu_weixing@qq.com>
2026-06-26 23:40:12 +0800

    update

--- ax-springboot2-demo/a02-spring-boot-db/a02-db-sys-mybatis/src/main/java/com/github/axinger/sys/service/impl/DepartmentChainWrapper.java
+++ ax-springboot2-demo/a02-spring-boot-db/a02-db-sys-mybatis/src/main/java/com/github/axinger/sys/service/impl/DepartmentChainWrapper.java
@@ -7,13 +7,23 @@ import com.github.axinger.sys.domain.DepartmentEntity;
 import com.github.axinger.sys.service.DepartmentService;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * 自定义链式查询包装器
- * 继承 AbstractChainWrapper 以获得 eq, like, in 等链式方法
+ * <p>
+ * 继承 AbstractChainWrapper，泛型 Children = DepartmentChainWrapper，
+ * 使 eq/like/in 等链式方法返回 DepartmentChainWrapper 类型，从而能调用自定义终端方法。
+ * <p>
+ * 调用方式：
+ * <pre>
+ *   service.chainQuery().eq(DepartmentEntity::getId, 1).listLeftSon();
+ *   service.chainQuery().like(DepartmentEntity::getName, "技术").listLeftSon(w -> w.eq(..., ...));
+ * </pre>
+ *
+ * @author xing
  */
-public class DepartmentChainWrapper
-        extends AbstractChainWrapper<DepartmentEntity, SFunction<DepartmentEntity, ?>, DepartmentChainWrapper, LambdaQueryWrapper<DepartmentEntity>> {
+public class DepartmentChainWrapper extends AbstractChainWrapper<DepartmentEntity, SFunction<DepartmentEntity, ?>, DepartmentChainWrapper, LambdaQueryWrapper<DepartmentEntity>> {
 
     private final DepartmentService service;
 
@@ -24,11 +34,17 @@ public class DepartmentChainWrapper
     }
 
     /**
-     * 自定义的终端方法：执行左连接查询
-     * 调用方式：service.chainQuery().eq(...).listLeftSon();
+     * 自定义终端方法：执行左连接查询（使用当前链式条件）
      */
     public List<DepartmentEntity> listLeftSon() {
-        // 获取构建好的 Wrapper 并传递给 Service
-        return service.listLeftSon(this.getWrapper());
+        return service.listLeftSon(getWrapper());
+    }
+
+    /**
+     * 自定义终端方法：执行左连接查询（额外追加 Consumer 条件）
+     */
+    public List<DepartmentEntity> listLeftSon(Consumer<LambdaQueryWrapper<DepartmentEntity>> consumer) {
+        consumer.accept((LambdaQueryWrapper<DepartmentEntity>) getWrapper());
+        return service.listLeftSon(getWrapper());
     }
 }
